a5bf5860001b53d1bfd0aa8d9be87845953c2e9e
Mark Campbell <markcampbell114@gmail.com>
2022-10-28 16:55:00 +0100

    Added similar movies list to movie details page

---
+++ src/components/similarMovieCard/index.js
@@ -0,0 +1,58 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardHeader from "@mui/material/CardHeader";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import img from '../../images/film-poster-placeholder.png'
+import React, { useContext  } from "react";
+import PersonIcon from '@mui/icons-material/Person';
+import { Link } from "react-router-dom";
+export default function SimilarMovieCard({ similarMovies }) {
+  return (
+    <Card sx={{ minWidth: 210 ,maxWidth: 345 }}>
+      <CardHeader
+        title={
+          <Typography variant="h5" component="p">
+            {similarMovies.title}{" "}
+          </Typography>
+        }
+      />
+      <CardMedia
+        sx={{ height: 500 }}
+        image={
+            similarMovies.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${similarMovies.poster_path}`
+            : img
+        }
+      />
+      <CardContent>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <CalendarIcon fontSize="small" />
+              {similarMovies.release_date}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <StarRateIcon fontSize="small" />
+              {"  "} {similarMovies.vote_average}{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Link to={`/movies/${similarMovies.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+}
