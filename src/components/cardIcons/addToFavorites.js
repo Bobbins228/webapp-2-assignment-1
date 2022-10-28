170001a70342f2d561f9fef2fbaf935d7a92187a
Mark Campbell <markcampbell114@gmail.com>
2022-10-28 16:02:32 +0100

    Users can no longer like movies without being logged in

--- src/components/cardIcons/addToFavorites.js
+++ src/components/cardIcons/addToFavorites.js
@@ -1,12 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../../firebase";
+
 
 const AddToFavoritesIcon = ({ movie }) => {
   const context = useContext(MoviesContext);
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
 
   const handleAddToFavorites = (e) => {
+    if (loading) return;
+    if (!user) return navigate("/");
     e.preventDefault();
     context.addToFavorites(movie);
   };
