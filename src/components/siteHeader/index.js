dffab26a00f0d1a6ed2c537881de3358d4a23709
Mark Campbell <60522925+Bobbins228@users.noreply.github.com>
2022-11-03 21:36:18 +0000

    Changed log out menu item

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -38,6 +38,9 @@ const SiteHeader = ({ history }) => {
   const handleMenuSelect = (pageURL) => {
     if (pageURL === "/"){
       logout()
+        .then(() => {
+          navigate("/", {replace: true});
+        });
     }
     navigate(pageURL, { replace: true });
   };
