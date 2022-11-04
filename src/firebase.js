e000029eb884b81d9e0d492769b839f322a6e86d
Mark Campbell <60522925+Bobbins228@users.noreply.github.com>
2022-11-04 00:21:01 +0000

    Fixed log out function in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -94,9 +94,13 @@ const sendPasswordReset = async (email) => {
   }
 };
 
-const logout = () => {
-  signOut(auth);
-};
+
+  const logout = () => {
+
+    return Promise.resolve(signOut(auth));
+  
+  };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
